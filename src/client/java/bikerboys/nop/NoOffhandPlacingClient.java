0857170ac0a26ec232cb8388c2e8b576354124d3
unknown <hornandrewm@gmail.com>
2025-11-01 12:33:16 -0500

    fix keybindings

--- src/client/java/bikerboys/nop/NoOffhandPlacingClient.java
+++ src/client/java/bikerboys/nop/NoOffhandPlacingClient.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
@@ -13,11 +14,15 @@ import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
 public class NoOffhandPlacingClient implements ClientModInitializer {
-
-	// public static KeyBinding enableKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("Disable Off Hand", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_Y, "Disable off Hand"));
-	public static boolean enabled = false;
-
-
+	public static final String MOD_ID = "no-offhand-placing";
+	public static KeyBinding enableKey = KeyBindingHelper.registerKeyBinding(
+		new KeyBinding(
+			"Disable Off Hand", 
+			InputUtil.Type.KEYSYM, 
+			GLFW.GLFW_KEY_Y, 
+			new KeyBinding.Category(Identifier.of(MOD_ID, "disable_offhand"))
+		)
+	);	public static boolean enabled = false;
 
 	@Override
 	public void onInitializeClient() {
@@ -29,19 +34,17 @@ public class NoOffhandPlacingClient implements ClientModInitializer {
 	private void onClientTick(MinecraftClient minecraftClient) {
 		PlayerEntity player = minecraftClient.player;
 		if(player == null) return;
-
-		enabled = true;
 		
-		// if(enableKey.wasPressed()) {
-		// 	minecraftClient.world.playSound(player, player.getBlockPos(), SoundEvents.UI_BUTTON_CLICK.value(), SoundCategory.MASTER, 0.7f, 1f);
+		if(enableKey.wasPressed()) {
+			minecraftClient.world.playSound(player, player.getBlockPos(), SoundEvents.UI_BUTTON_CLICK.value(), SoundCategory.MASTER, 0.7f, 1f);
 
-		// 	enabled = !enabled;
-		// 	if(enabled) {
-		// 		player.sendMessage(Text.of("Off Hand Disabled"), true);
-		// 	} else {
-		// 		player.sendMessage(Text.of("Off Hand Enabled"), true);
-		// 	}
-		// }
+			enabled = !enabled;
+			if(enabled) {
+				player.sendMessage(Text.of("Off Hand Disabled"), true);
+			} else {
+				player.sendMessage(Text.of("Off Hand Enabled"), true);
+			}
+		}
 	}
 
 
